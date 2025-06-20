b8ac497c8f9e25fd8f49cfea5f7440dccfcd3516
Shayan <shayanhorbank@gmail.com>
2025-06-20 20:57:49 +0200

    Add: PWA support

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['kartei-logo.webp'],
+      manifest: {
+        name: 'Kartei Flashcards',
+        short_name: 'Kartei',
+        description: 'Kartei – just another flashcard app',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      }
+    })
+  ]
+});
